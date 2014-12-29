1255fa5b6b2c221055435f9680bfe7213b251cf5
wgabsi88 <wael.gabsi@rocket-internet.de>
2014-12-29 18:01:29 +0100

    Update calabash_steps.rb

--- features/step_definitions/calabash_steps.rb
+++ features/step_definitions/calabash_steps.rb
@@ -7,31 +7,23 @@ Then(/^I see the "([^\"]*)"$/) do |displayed_label|
 
     when "Get started screen"
       var_text = "* marked:'GET STARTED'"
-      
     when "Skip"
       var_text = "textview  id:'tv_fragment_carousel_skip'"
-    
     when "Zipcode screen"
       var_text = "* marked:'Go'"
 
     when "New order screen"
       var_text = "* id:'btn_fragment_new_order_get_cleaned_clothes'"
-    
     when"Keep me Posted"
       var_text = "* id:'btn_fragment_keep_me_posted'  marked:'KEEP ME POSTED'"
-    
     when "Contact info screen"
       var_text = "* id:'et_fragment_add_contact_info_name'"
-    
     when "Address screen"
       var_text = "* id:'et_fragment_address_line_one'"
-    
     when "Credit card screen"
       var_text = "* id:'et_fragment_add_credit_card_number'"
-    
     when "Order Summary screen"
       var_text = "* id:'btn_fragment_order_summary_get_cleaned_clothes'"
-    
     when "Order confirmation screen"
       var_text = "* marked:'Your Order\nis Confirmed!'"
   end
@@ -44,7 +36,7 @@ Then (/^I touch the "(.*?)" button$/) do |select|
   case select
     when "Start"
       touch("* {text CONTAINS 'GET STARTED'}")
-    
+   
     when "Skip"
       tap_when_element_exists("*  id:'tv_fragment_carousel_skip'", :timeout => 5)
     
@@ -55,13 +47,13 @@ Then (/^I touch the "(.*?)" button$/) do |select|
       touch("*   marked:'Go'")
     
     when "Got it"
-      tap_when_element_exists("*  id:'btn_dialog_price_list_ok'")
-   
+       tap_when_element_exists("*  id:'btn_dialog_price_list_ok'")
+    
     when "Email"
-      touch("*   id:'et_fragment_no_service_email_address'")
+       tap_when_element_exists("*   id:'et_fragment_no_service_email_address'")
    
     when "Keep me Posted"
-      tap_when_element_exists("*  id:'btn_fragment_keep_me_posted'", :timeout => 5)
+       tap_when_element_exists("*  id:'btn_fragment_keep_me_posted'")
      
     when "Full Name"
       touch("*   id:'et_fragment_add_contact_info_name'")
@@ -107,9 +99,11 @@ Then (/^I touch the "(.*?)" button$/) do |select|
     
     when"Date List"
       touch ("* id:'lv_fragment_time_picker_date'")
-    
+   
     when "Time List"
       touch ("* id:'lv_fragment_time_picker_time'")
+
+
   end
 
 end
@@ -124,27 +118,22 @@ Then(/^I enter "(.*?)"$/) do |text_input|
     when "a covered zipcode" #in Berlin
       text_to_input = '10117'
 
-    when "an Email Address"
+     when "an Email Address"
        text_to_input = Array.new(16){[*'0'..'9', *'a'..'z'].sample}.join + '@example.com'
 
     when "a Phone Number"
       text_to_input = Array.new(16){[*'0'..'9', *'a'..'z'].sample}.join
-   
     when "any name"
           text_to_input = Array.new(8){[*'a'..'z'].sample}.join
-   
     when "an address"
       text_to_input = Array.new(5){[*'a'..'z'].sample}.join
-   
     when "a credit card number"
       text_to_input = "4111111111111111"
-   
     when "expiration month"
       text_to_input = "06"
 
     when "expiration year"
       text_to_input = "16"
-   
     when "CVC"
       text_to_input = "737"
 
